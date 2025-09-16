df73bc54a8aff408eff7ff5342e6119590e3be6b
Rynetz <ryanpratama454545@gmail.com>
2025-09-17 03:22:18 +0700

    tugas 3

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,8 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-     path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
-     path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
+     path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+     path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
      path('json/', show_json, name='show_json'),
      path('xml/', show_xml, name='show_xml'),
      path('create-product/', create_product, name='create_product'),
